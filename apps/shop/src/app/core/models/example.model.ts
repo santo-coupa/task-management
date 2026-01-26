17f28b159f5438aac176cc0fee8086ac4e7619e6
Samir <samir.osmanovic@cirtuo.com>
2026-01-26 17:20:53 +0100

    Code refactor

--- apps/shop/src/app/core/models/example.model.ts
+++ apps/shop/src/app/core/models/example.model.ts
@@ -6,3 +6,14 @@ export enum ExampleEnum {
     FIRST = 'first',
     SECOND = 'second',
 }
+
+/**
+ * Represents logged-in user data
+ * (extend this later with roles, permissions, etc.)
+ */
+export interface AuthUser {
+    id: string;
+    email: string;
+    token: string;
+  }
+  
