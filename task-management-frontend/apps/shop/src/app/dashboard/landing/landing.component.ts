f2dc45323fe73d40d349c605428e87c99513687d
Santosh Yadav <santosh.yadav@coupa.com>
2026-01-27 13:43:38 +0530

    Initial project setup

---
+++ task-management-frontend/apps/shop/src/app/dashboard/landing/landing.component.ts
@@ -0,0 +1,7 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-dashboard-landing',
+//   templateUrl: './landing.component.html',
+// })
+// export class LandingComponent {}
