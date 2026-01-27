906a91d64a31217cc4bb0bce2386b493d9485ce4
Santosh Yadav <santosh.yadav@coupa.com>
2026-01-27 15:44:55 +0530

    Revert "Setup/initial project"

--- task-management-backend/Database/User.cs
+++
@@ -1,9 +0,0 @@
-namespace task_management_backend.Database;
-
-public class User
-{
-    public Guid ID { get; set; }
-    public string Username { get; set; }
-    public string PasswordHashed { get; set; }
-    public string Email { get; set; }
-}
