8cff31573f7754e166f20d942cd5ea91c2b08f95
Santosh Yadav <santosh.yadav@coupa.com>
2026-01-29 10:35:05 +0530

    dashboard feature added

--- task-management-frontend/src/app/app.routes.ts
+++ task-management-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './auth/auth.routes';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
 export const routes: Routes = [
   ...authRoutes,
+  ...dashboardRoutes,
 ];
