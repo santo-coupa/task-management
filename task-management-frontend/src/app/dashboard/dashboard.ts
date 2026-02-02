f1a234c13289c1316c71470e83ff476ec7bc31f5
Santosh Yadav <santosh.yadav@coupa.com>
2026-02-02 15:19:39 +0530

    feat(dashboard) : redesign dashboard, added hardcoded dummy data

--- task-management-frontend/src/app/dashboard/dashboard.ts
+++ task-management-frontend/src/app/dashboard/dashboard.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class DashboardComponent {
-
+  stats = [
+    {
+      title: 'Total Tasks',
+      value: 42,
+      icon: 'pi pi-list',
+    },
+    {
+      title: 'Completed Tasks',
+      value: 28,
+      icon: 'pi pi-check-circle',
+    },
+    {
+      title: 'Pending Tasks',
+      value: 14,
+      icon: 'pi pi-clock',
+    },
+    {
+      title: 'Users',
+      value: 5,
+      icon: 'pi pi-users',
+    },
+  ];
 }
