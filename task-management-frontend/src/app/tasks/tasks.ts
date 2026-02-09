23185858e51fa7e4e0566375637dd2bce53ec373
Santosh Yadav <santosh.yadav@coupa.com>
2026-02-09 15:36:42 +0530

    feat(tasks) : add task ui

--- task-management-frontend/src/app/tasks/tasks.ts
+++ task-management-frontend/src/app/tasks/tasks.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TableModule } from 'primeng/table';
+
+import { UserTask } from '../core/models/task.model';
+import { TaskService } from '../core/services/task.service';
+import { AuthService } from '../core/services/auth.service';
+import { Role } from '../core/models/role.enum';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TableModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss',
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  tasks: UserTask[] = [];
+  isAdmin = false;
 
+  constructor(
+    private taskService: TaskService,
+    private authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    this.isAdmin = this.authService.hasRole(Role.ADMIN);
+    this.tasks = this.taskService.getTasks();
+  }
 }
