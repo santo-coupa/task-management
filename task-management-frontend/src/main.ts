80100bb025965f97c1f2fb3f6f0ba97b03d955d8
Santosh Yadav <santosh.yadav@coupa.com>
2026-02-13 22:24:21 +0530

    feat(interceptor): add auth interceptor

--- task-management-frontend/src/main.ts
+++ task-management-frontend/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './app/core/interceptors/auth.interceptor';
 
 import { AppComponent } from './app/app';
 import { routes } from './app/app.routes';
@@ -15,6 +16,8 @@ bootstrapApplication(AppComponent, {
         preset: Aura
       }
     }),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 }).catch(err => console.error(err));
